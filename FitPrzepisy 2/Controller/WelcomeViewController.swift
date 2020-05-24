61bd31988950df76fde578c04c536d1418fdad94
Pawel Slusarz <pawel.slusarz05@gmail.com>
2020-05-24 19:30:15 +0200

    Creating app + cocoapods

---
+++ FitPrzepisy 2/Controller/WelcomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FitPrzepisy 2
+//
+//  Created by zari on 24/05/2020.
+//  Copyright © 2020 zari. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func registerButtonPressed(_ sender: UIButton) {
+    }
+    
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '13.0'
+
+target 'FitPrzepisy 2' do
+  use_frameworks!
+
+  # Pods for FitPrzepisy 2
+  
+  pod 'CLTypingLabel'
+
+end
